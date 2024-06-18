d38f0f5f6ebcb4be6ef5ca34075d31e3ee021f40
Tech Wizard <christiaan93210@gmail.com>
2024-06-19 04:25:19 +0900

    completed carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Carousel from "./components/Carousel";
+
+import "./App.css";
+
+import slide1 from "./assets/images/slide1.png";
+import slide2 from "./assets/images/slide2.png";
+import slide3 from "./assets/images/slide3.png";
+import slide4 from "./assets/images/slide4.png";
+import slide5 from "./assets/images/slide4.png";
+
 
 function App() {
+  const [visibleItemsCount, setVisibleItemsCount] = React.useState(3);
+
+  const handleResize = () => {
+    if (window.innerWidth < 768) {
+      setVisibleItemsCount(1);
+    } else {
+      setVisibleItemsCount(3);
+    }
+  };
+
+  React.useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const slides = [slide1, slide2, slide3, slide4, slide5];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Carousel visibleItemsCount={visibleItemsCount} withIndicator isInfinite>
+        {slides.map((slide, index) => {
+          return (
+            <div key={index} className="slide-container">
+              <img src={slide} alt="slide" />
+            </div>
+          );
+        })}
+      </Carousel>
     </div>
   );
 }
